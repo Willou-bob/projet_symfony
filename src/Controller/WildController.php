9f85af5cbc360efc413237ec22da8546ddcc5a78
Florentin Wilfried <florentinwilfried@gmail.com>
2020-03-10 17:44:32 +0100

    creation 2 fichiers dans la vue index et show suivie de leurs controleur avec leur route

---
+++ src/Controller/WildController.php
@@ -0,0 +1,51 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * Getting a program with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="show_program")
+     * @return Response
+     */
+    public function showByProgram(?string $slug) :Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        if (!$slug) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+
+            'slug'  => $slug,
+        ]);
+    }
+}
+
