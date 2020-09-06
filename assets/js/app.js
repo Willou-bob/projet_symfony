b55b83efcb5d425a3c11da1e99cedd9d1c35dde6
Florentin Wilfried <florentinwilfried@gmail.com>
2020-09-06 19:20:45 +0200

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,12 @@
 // app.js
 
-// any CSS you require will output into a single css file (app.scss in this case)
-require('./app.scss');
-
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
+// any CSS you require will output into a single css file (app.scss in this case)
+require('../scss/app.scss');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
@@ -15,3 +14,5 @@ require('bootstrap');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+
